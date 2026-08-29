dc0e74e8ffa67eccfcc9a04058d69943c97ee3e8
Ronnel Tabanera <ronnel.tabanera@gmail.com>
2026-08-29 08:32:11 +0800

    fix auth js

--- auth.js
+++ auth.js
@@ -1,27 +1,19 @@
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
-
 module.exports.createAccessToken = (user) => {
 	const data = {
 		id: user._id,
 		email: user.email,
 		isAdmin: user.isAdmin
 	};
-
 	return jwt.sign(data, process.env.JWT_KEY, {});
 }
-
 module.exports.userVerification = (req, res, next) => {
-	console.log(req.headers.authorization);
 	let token = req.headers.authorization;
-
 	if(typeof token === 'undefined') {
-		return res.send({auth: "Failed. No Token"})
+		return res.status(401).send({ auth: "Failed", message: "No Token" });
 	} else {
-		console.log(token);
 		token = token.slice(7);
-		console.log(token);
-
 		jwt.verify(token, process.env.JWT_KEY, (err, decodedToken) => {
 			if(err) {
 				return res.status(403).send({
@@ -29,15 +21,12 @@ module.exports.userVerification = (req, res, next) => {
 					message: err.message
 				})
 			} else {
-				console.log("result from verify method: ");
-				console.log(decodedToken);
 				req.user = decodedToken;
 				next();
 			}
 		})
 	}
 }
-
 module.exports.verifyAdmin = (req, res, next) => {
 	if(req.user.isAdmin){
 		next();
@@ -48,11 +37,9 @@ module.exports.verifyAdmin = (req, res, next) => {
 		})
 	}
 }
-
 module.exports.errorHandler = (err, req, res, next) => {
 	console.log("ERROR FOUND:");
 	console.log(err);
-
 	const errorMessage = err.message || 'Internal Server Error';
 	res.status(500).json({
 		error: {
